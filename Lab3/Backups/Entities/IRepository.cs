13f181b8e059c5f8d5bc1de4a53fba52b666d269
zdarovayrodi <chopmhop@yandex.ru>
2022-11-25 19:06:36 +0300

    refactor: change repository - GetStream func

--- Lab3/Backups/Entities/IRepository.cs
+++ Lab3/Backups/Entities/IRepository.cs
@@ -1,17 +1,7 @@
 namespace Backups.Entities
 {
-    /// <summary>
-    /// Файл просто предоставляет доступ к какому-то объекту (стриму данных), на который указывает пользователь
-    /// пример:
-    /// Юзер: я хочу достать файл по такому-то пути
-    /// Репа: отдает стрим
-    /// Юзер: спасибо
-    /// </summary>
     public interface IRepository
     {
-        IReadOnlyList<string> Files { get; }
-        IReadOnlyList<string> Directories { get; }
-        void AddFile(string filePath);
-        void AddDirectory(string directoryPath);
+        Stream GetStream(string path);
     }
 }
--- Lab3/Backups/Entities/Repository.cs
+++ Lab3/Backups/Entities/Repository.cs
@@ -4,49 +4,11 @@ namespace Backups.Entities
 
     public class Repository : IRepository
     {
-        private List<string> _files;
-        private List<string> _directories;
-
-        public Repository()
+        public Stream GetStream(string path)
         {
-            _files = new List<string>();
-            _directories = new List<string>();
-        }
-
-        public IReadOnlyList<string> Files => _files.AsReadOnly();
-        public IReadOnlyList<string> Directories => _directories.AsReadOnly();
-
-        // return file stream 
-        public Stream GetFile(string name)
-        {
-            if (!_files.Contains(name))
-            {
-                throw new FileDoesNotExistException();
-            }
-
-            return File.OpenRead(name);
-        }
-
-        public void AddFile(string filePath)
-        {
-            if (string.IsNullOrWhiteSpace(filePath))
-                throw new RepositoryException("Value cannot be null or whitespace");
-            if (!File.Exists(filePath))
-                throw new RepositoryException("File does not exist");
-            if (_files.Contains(filePath))
-                throw new RepositoryException("File already added");
-            _files.Add(filePath);
-        }
-
-        public void AddDirectory(string directoryPath)
-        {
-            if (string.IsNullOrWhiteSpace(directoryPath))
-                throw new RepositoryException("Value cannot be null or whitespace");
-            if (!Directory.Exists(directoryPath))
-                throw new RepositoryException("Directory does not exist");
-            if (_directories.Contains(directoryPath))
-                throw new RepositoryException("Directory already added");
-            _directories.Add(directoryPath);
+            if (!File.Exists(path))
+                throw new RepositoryException("File not found");
+            return new FileStream(path, FileMode.Open, FileAccess.Read);
         }
     }
 }
