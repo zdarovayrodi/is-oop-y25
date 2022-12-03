09247c04b0ea3ae31cc0046ba37ced52aa2092d8
zdarovayrodi <chopmhop@yandex.ru>
2022-12-03 17:23:37 +0300

    feat: add Transaction class

---
+++ Lab4/Banks/Models/Trasaction.cs
@@ -0,0 +1,29 @@
+namespace Banks.Models
+{
+    using Banks.Accounts.Interfaces;
+
+    public class Trasaction
+    {
+        public Trasaction(IAccount from, IAccount to, decimal amount)
+        {
+            if (from == null || to == null)
+            {
+                throw new ArgumentNullException("Account cannot be null");
+            }
+
+            From = from;
+            To = to;
+            Amount = amount;
+        }
+
+        public IAccount From { get; }
+        public IAccount To { get; }
+        public decimal Amount { get; }
+
+        public void Revert()
+        {
+            From.Withdraw(Amount);
+            To.Deposit(Amount);
+        }
+    }
+}
