66fb6599c840822375fa54ba25af129509820b09
zdarovayrodi <chopmhop@yandex.ru>
2022-12-10 14:44:01 +0300

    refactor: add Banks and Clients setters

--- Lab4/Banks/Entities/Interfaces/ICentralBank.cs
+++ Lab4/Banks/Entities/Interfaces/ICentralBank.cs
@@ -4,6 +4,9 @@ namespace Banks.Entities.Interfaces;
 
 public interface ICentralBank
 {
+    IReadOnlyList<Bank> Banks { get; }
+    IReadOnlyList<Client> Clients { get; }
+    Client RegisterClient(string name, string surname, string passportNumber, string address);
     Bank CreateBank(string name, List<DepositInterestRates> depositRates, decimal debitRate, decimal avaliableSumIfSuspicious);
     void RewindTime(uint amountOfMonths);
 }
