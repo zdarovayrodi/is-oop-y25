cbae211ded17bad8d970f3868a46b297d1c8f6c8
zdarovayrodi <chopmhop@yandex.ru>
2022-12-10 03:27:31 +0300

    test: add revert transaction test

--- Lab4/Banks.Test/Tests.cs
+++ Lab4/Banks.Test/Tests.cs
@@ -31,8 +31,30 @@ namespace Banks.Test
         }
 
         [Fact]
-        public void Test1()
+        public void TestRevertTransaction()
         {
+            var lowInterest = new DepositInterestRates(10000, 10);
+            var medInterest = new DepositInterestRates(100000, 15);
+            var highInterest = new DepositInterestRates(1000000, 20);
+            List<DepositInterestRates> ratesList = new List<DepositInterestRates>();
+            ratesList.Add(lowInterest);
+            ratesList.Add(medInterest);
+            ratesList.Add(highInterest);
+            var debitRate = 10;
+            var testBank1 = centralBank.CreateBank("Test Bank 1", ratesList, debitRate);
+            var testClient1 = new Client("Test", "Testoviy");
+            var testClient2 = new Client("Test2", "Testoviy");
+            testBank1.AddClient(testClient1);
+            testBank1.AddClient(testClient2);
+            var testAccount1 = testBank1.DebitAccounts.First();
+            var testAccount2 = testBank1.DebitAccounts.Last();
+            testAccount1.Deposit(1000);
+            Assert.Equal(1000, testAccount1.Balance);
+            var transaction = testAccount1.Transfer(testAccount2, 500);
+            Assert.Equal(500, testAccount1.Balance);
+            Assert.Equal(500, testAccount2.Balance);
+            testAccount1.RevertTransaction(transaction);
+            Assert.Equal(1000, testAccount1.Balance);
         }
     }
 }
