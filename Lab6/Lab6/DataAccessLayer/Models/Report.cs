2ef7a60f9b7bf64ec5f78b8c0b269eaca80b57ac
zdarovayrodi <chopmhop@yandex.ru>
2022-12-29 21:34:31 +0300

    feat: add Report class

--- Lab6/Lab6/DataAccessLayer/Models/Report.cs
+++ Lab6/Lab6/DataAccessLayer/Models/Report.cs
@@ -1,5 +1,14 @@
+using Lab6.DataAccessLayer.Entities;
+
 namespace Lab6.DataAccessLayer.Models;
 
 public class Report
 {
+    public Report() { Id = Guid.NewGuid(); }
+    public Guid Id { get; set; }
+    public uint CountOfProcessedMessages { get; set; }
+    public IReadOnlyList<Message> Messages { get; set; }
+    public int GetProcessedMessegasCount() => Messages.Count(msg => msg.IsProcessed);
+    public int GetMessagesCount(Device device) => Messages.Count(msg => msg.Device == device);
+    public int GetMessagesCount(DateTime start, DateTime end) => Messages.Count(msg => msg.DateTime >= start && msg.DateTime <= end);
 }
