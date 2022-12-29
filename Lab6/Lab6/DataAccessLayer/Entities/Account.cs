f8e4bce54383639eee8bc2c2e196aab8d9e703f2
zdarovayrodi <chopmhop@yandex.ru>
2022-12-30 01:24:27 +0300

    feat: add Account realisation

--- Lab6/Lab6/DataAccessLayer/Entities/Account.cs
+++ Lab6/Lab6/DataAccessLayer/Entities/Account.cs
@@ -1,5 +1,20 @@
+using Lab6.DataAccessLayer.Models;
+
 namespace Lab6.DataAccessLayer.Entities;
 
 public class Account
 {
+    private string _password;
+    public Account(string name, string password, AccountStatus status)
+    {
+        Name = name;
+        _password = password;
+        Status = status;
+    }
+    
+    public string Name { get; set; }
+    public string Password => _password;
+    public string Email { get; set; }
+    public string Messenger { get; set; }
+    public AccountStatus Status { get; set; }
 }
