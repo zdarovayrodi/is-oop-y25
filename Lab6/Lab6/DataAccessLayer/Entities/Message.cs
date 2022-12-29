c7ec4702ca11b94001d09ced0a1fd61d1f6c8f89
zdarovayrodi <chopmhop@yandex.ru>
2022-12-30 01:25:11 +0300

    feat: add Message

--- Lab6/Lab6/DataAccessLayer/Entities/Message.cs
+++ Lab6/Lab6/DataAccessLayer/Entities/Message.cs
@@ -1,5 +1,21 @@
+using Lab6.DataAccessLayer.Models;
+
 namespace Lab6.DataAccessLayer.Entities;
 
 public class Message
 {
+    public Message(string text, DateTime date, Account from)
+    {
+        Text = text;
+        TimeStamp = date;
+        From = from;
+        Id = Guid.NewGuid();
+    }
+    public Guid Id { get; }
+    public Account From { get; set; }
+    public string Text { get; set; }
+    public bool IsProcessed { get; set; }
+    public DateTime TimeStamp { get; set; }
+    public MessageSource SourceFrom { get; private set; }
+    public MessageSource ProcessedFrom { get; private set; }
 }
