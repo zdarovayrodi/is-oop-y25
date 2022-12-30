4901cba072799b0625807a5a52ba7630ec5c3b30
zdarovayrodi <chopmhop@yandex.ru>
2022-12-30 05:19:20 +0300

    refactor: fix misunderstatement

--- Lab6/DataAccessLayer/Models/Report.cs
+++ Lab6/DataAccessLayer/Models/Report.cs
@@ -4,7 +4,7 @@ namespace Lab6.Models;
 
 public class Report
 {
-    private readonly List<Message> _messages;
+    private readonly List<Message>? _messages;
     public Report(List<Message> messages)
     {
         _messages = messages;
@@ -15,7 +15,7 @@ public class Report
     public IReadOnlyList<Message> Messages { get; set; }
     public int GetProcessedMessagesCount()
         => Messages.Count(msg => msg.IsProcessed);
-    public int GetMessagesCount(MessageSource processedFrom)
+    public int GetMessagesCount(Account processedFrom)
         => Messages.Count(msg => msg.ProcessedFrom == processedFrom);
     public int GetMessagesCount(DateTime start, DateTime end)
         => Messages.Count(msg => msg.TimeStamp >= start && msg.TimeStamp <= end);
