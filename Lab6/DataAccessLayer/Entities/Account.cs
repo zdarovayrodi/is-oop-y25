a7bb485739d4d1a4168931ef1dfb82fd3d2ede5e
zdarovayrodi <chopmhop@yandex.ru>
2022-12-30 05:48:22 +0300

    feat: add subordinates

--- Lab6/DataAccessLayer/Entities/Account.cs
+++ Lab6/DataAccessLayer/Entities/Account.cs
@@ -5,6 +5,7 @@ namespace Lab6.Entities;
 public class Account
 {
     private string _password;
+    private List<Account> _subordinates = new List<Account>(); 
     public Account(string username, string password, AccountStatus status)
     {
         Username = username;
@@ -16,5 +17,12 @@ public class Account
     public string? Email { get; set; }
     public string? Messenger { get; set; }
     public AccountStatus Status { get; set; }
+    public IReadOnlyList<Account> Subordinates => _subordinates.AsReadOnly();
     public bool IsCorrectPassword(string password) => _password == password;
+    
+    public void AddSubordinate(Account subordinate)
+    {
+        if (subordinate == null) throw new Exception("Account is null");
+        _subordinates.Add(subordinate);
+    }
 }
